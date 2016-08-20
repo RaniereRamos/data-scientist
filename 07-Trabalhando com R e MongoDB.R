2810e2fa748b6e38bd86359d59bc598b94ad9b38
Raniere Ramos <ramos.raniere@gmail.com>
2016-08-20 15:31:21 -0300

    Add files via upload

--- 07-Trabalhando com R e MongoDB.R
+++ 07-Trabalhando com R e MongoDB.R
@@ -18,6 +18,61 @@ install.packages("rmongodb")    # It didn't work
 install.packages("rmongodb", repo = "http://nbcgib.uesc.br/mirrors/cran")
 library(rmongodb)
 
+# Creating a connection
+help("mongo.create")
+mongo <- mongo.create()
+mongo
+
+# Checking connection
+mongo.is.connected(mongo)
+
+# List Mongo's databases
+if (mongo.is.connected(mongo) == TRUE){
+   mongo.get.databases(mongo)
+}
+
+# Armazenando o nome de uma das colecoes / 
+# Storing the name of one of the collections
+coll <- "users.contatos"
+
+# Contando os registros em uma colecao / Counting records in a collection
+help("mongo.count")
+mongo.count(mongo,coll)
+
+# Buscando um registro em uma colecao / Searching a record in a collection
+mongo.find.one(mongo,coll)
+
+# Obtendo um vetor de valores distintos das chaves de uma colecao
+# Getting a vector of distinct values of the keys of a collection
+res <- mongo.distinct(mongo, coll, "city")
+head(res)
+
+# Obtendo um vetor de valores distintos das chaves de uma colacao e gerando histograma
+# Getting a vector of distinct values of the keys of a collection and making a hist
+pop <- mongo.distinct(mongo, coll, "pop")
+hist(pop)
+
+# Contando os elementos / Counting elements
+nr <- mongo.count(mongo, coll, list("pop" = list("$lte" = 2)))
+print(nr)
+
+# Buscando todos os elementos / Searching all elements
+pops <- mongo.find.all(mongo, coll, list("pop" = list("$lte" = 2)))
+head(pops,2)
+
+# Encerrando a conexao / Closing the connection
+mongo.destroy(mongo)
+
+
+# Criando e validando um arquivo json
+library(jsonlite)
+json_arquivo <- '{"pop":{"$lte":2}, "pop":{"$gte":1}}'
+cat(prettify(json_arquivo))
+
+validate(json_arquivo)
+
+
+
 
 
 
