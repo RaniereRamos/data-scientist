4442ddeba17183acad0b3c9ccafeadcb9cd4c246
Raniere Ramos <ramos.raniere@gmail.com>
2016-08-28 11:20:27 -0300

    Add files via upload

---
+++ 04 - Addins.R
@@ -0,0 +1,41 @@
+###############################################################################
+#                                                                             #
+# CURSO:  Formacao Cientista de Dados        PLATAFORMA: Data Science Academy #
+# MODULO: Big Data Analytics com R           INSTRUTOR:  Daniel Mendes        #
+#                                                                             #
+###############################################################################
+
+
+## Usando o add-in GREA / Using the add-in GREA
+
+# Que tal uma interface gráfica para importar seus arquivos
+# How about a graphical interface to import your files
+
+install.packages("devtools")
+library(devtools)
+
+devtools::install_github("Stan125/GREA")
+
+# Clique em : Tools --> Addins --> Gotta Read Em All
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
